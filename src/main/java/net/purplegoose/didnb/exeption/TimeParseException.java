222be69895d9764527659a1db4ad5ce3c7d00e06
Umbreon Majora <umbreon.majora@gmail. om>
2023-08-27 09:06:47 +0200

    Code refactoring & translated more phrases

---
+++ src/main/java/net/purplegoose/didnb/exeption/TimeParseException.java
@@ -0,0 +1,7 @@
+package net.purplegoose.didnb.exeption;
+
+public class TimeParseException extends RuntimeException {
+    public TimeParseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
