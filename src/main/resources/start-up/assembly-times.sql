b606ee484d1472770d719a9cd3bf4d6cd41de7b7
Umbreon <umbreon.majora@gmail.com>
2023-09-20 09:14:40 +0200

    Added new assembly times

--- src/main/resources/start-up/assembly-times.sql
+++ src/main/resources/start-up/assembly-times.sql
@@ -5,5 +5,11 @@ VALUES
 ('18:45-19:00', 'Wednesday', 3),
 ('18:45-19:00', 'Thursday', 4),
 ('18:45-19:00', 'Friday', 5),
-('18:45-19:00', 'Saturday', 6)
+('18:45-19:00', 'Saturday', 6),
+('11:45-12:00', 'Monday', 7),
+('11:45-12:00', 'Tuesday', 8),
+('11:45-12:00', 'Wednesday', 9),
+('11:45-12:00', 'Thursday', 10),
+('11:45-12:00', 'Friday', 11),
+('11:45-12:00', 'Saturday', 12)
 on duplicate key update id=id;
